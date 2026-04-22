8c21823c846874f7862f9dc11056a28190b08b61
Ofek Lamay <ofeklamay345@gmail.com>
2026-04-22 21:56:31 +0300

    Adding base java files

---
+++ Ofek_Lamay.java
@@ -0,0 +1,8 @@
+public class Ofek_Lamay {
+    public static void main(String[] args) {
+        for (int i=1; i <= 3; i++) {
+        System.out.println("Ofek Lamay ... "+i);
+        }
+        System.out.println("Finished");
+    }
+}
