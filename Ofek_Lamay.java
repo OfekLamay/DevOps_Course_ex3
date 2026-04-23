8c3acf25ad3d12c294e1fe57194784d2931963db
Ofek Lamay <ofeklamay345@gmail.com>
2026-04-23 15:59:40 +0300

    Changed message count to 4 times

--- Ofek_Lamay.java
+++ Ofek_Lamay.java
@@ -1,6 +1,6 @@
 public class Ofek_Lamay {
     public static void main(String[] args) {
-        for (int i=1; i <= 3; i++) {
+        for (int i=1; i <= 4; i++) {
         System.out.println("Ofek Lamay ... "+i);
         }
         System.out.println("Finished");
